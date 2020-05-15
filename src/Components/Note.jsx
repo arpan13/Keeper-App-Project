3225aec1454668893e08cac0bbb8e8b26f4efda2
Arpan Kumar Pal <58396070+arpan13@users.noreply.github.com>
2020-05-15 07:42:56 +0530

    Added header,footer,note and applied css styling to it

---
+++ src/Components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Note(){
+  const Note=(
+       <div className="note ">
+          <h1>Title</h1>
+           <p>Write Some Content Here</p>
+      </div>
+     
+     );
+    return Note;
+
+}
+export default Note;
